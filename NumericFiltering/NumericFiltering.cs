584dccb95a27bf2adfd85f378f8a64408e39ec18
Alan <official.alanroach@outlook.com>
2019-03-19 14:47:49 -0400

    Updated functions and added summaries.

---
+++ NumericFiltering/NumericFiltering.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class TextInput
+{
+    public string value;
+    public virtual void Add(char c)
+    {
+        value += c;
+    }
+    public string GetValue()
+    {
+        return value.ToString();
+    }
+}
+
+//if input is of type str or char, ignore
+public class NumericInput : TextInput
+{
+    override public void Add(char c)
+    {
+        String s = new string(c, 1);
+        int i = 0;
+        bool result = int.TryParse(s, out i);
+        if (result)
+        {
+            value += c;
+        }
+    }
+}
+
+public class UserInput
+{
+    public static void Main(string[] args)
+    {
+        TextInput input = new NumericInput();
+        input.Add('1');
+        input.Add('a');
+        input.Add('0');
+        input.Add('f');
+        Console.WriteLine(input.GetValue());
+        Console.ReadKey();
+    }
+}
---
+++ SmallProjects/Palindrome.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Palindrome
+{
+    /// <summary>
+    /// Reverses the given string.
+    /// </summary>
+    /// <param name="word"> Contains the word to be reversed.</param>
+    /// <returns></returns>
+    public static string ReverseString(string word)
+    {
+        
+        string reversedWord = "";
+
+        foreach (char c in word)
+        {
+            reversedWord = c + reversedWord;
+        }
+
+        return reversedWord;
+    }
+    /// <summary>
+    /// Checks to see if the given word is a palindrome.
+    /// </summary>
+    /// <param name="word"> Contains the string that is to be checked. </param>
+    /// <returns></returns>
+    public static bool IsPalindrome(string word)
+    {
+        word = word.ToLower();
+        string wordReversed = ReverseString(word);
+
+        return (word == wordReversed);
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome("Racecar"));
+        Console.ReadKey();
+    }
+}
