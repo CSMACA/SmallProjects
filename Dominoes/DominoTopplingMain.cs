a18fa067d63a0ff786a95ebcc15ffdf658dcf6b7
Alan <official.alanroach@outlook.com>
2019-04-08 14:15:08 -0400

    Added Domino algorithm

--- Dominoes/DominoTopplingMain.cs
+++ Dominoes/DominoTopplingMain.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dominoes
 {
@@ -26,105 +24,77 @@ namespace Dominoes
 
         static string INITIAL;
 
-        static int I;
-
         static List<Domino> dominoList = new List<Domino>();
-        static char previous { get { return dominoList[I - 1].Direction; } set { dominoList[I - 1].Direction = value; } }
-        static char next { get { return dominoList[I + 1].Direction; } set { dominoList[I + 1].Direction = value; } }
-
 
         static void Main(string[] args)
         {
             Console.WriteLine("Enter input string: ");
-            INITIAL = Console.ReadLine();
-            INITIAL.ToList().ForEach(x => dominoList.Add(new Domino(x)));
-
-            Console.Write("Before: ");
-            dominoList.ForEach(x => Console.Write(x.Direction));
-
-            List<int> ls = new List<int>();
-            List<int> rs = new List<int>();
-
-            do
+            INITIAL = Console.ReadLine().ToUpper();
+            if ((INITIAL.Contains('L') && INITIAL.Contains('R') && INITIAL.Contains('.')))
             {
-                ls = new List<int>();
-                rs = new List<int>();
+                INITIAL.ToList().ForEach(x => dominoList.Add(new Domino(x)));
 
-                //Get indexes of L's and R's
-                for (int k = 0; k < dominoList.Count; k++)
+                Console.Write("Before: ");
+                dominoList.ForEach(x => Console.Write(x.Direction));
+
+                List<int> ls = new List<int>();
+                List<int> rs = new List<int>();
+
+                do
                 {
-                    if (dominoList[k].Direction == 'L') ls.Add(k);
-                    if (dominoList[k].Direction == 'R') rs.Add(k);
-                }
+                    ls = new List<int>();
+                    rs = new List<int>();
 
-                //Decrement L's
-                //ls.ForEach(x => ls[x]--);
+                    //Get indexes of L's and R's
+                    for (int k = 0; k < dominoList.Count; k++)
+                    {
+                        if (dominoList[k].Direction == 'L') ls.Add(k);
+                        if (dominoList[k].Direction == 'R') rs.Add(k);
+                    }
 
-                for (int i = 0; i < ls.Count; i++)
-                {
-                    ls[i]--;
-                }
+                    //Decrement L's
+                    //ls.ForEach(x => ls[x]--);
 
-                //Increment R's
-                //rs.ForEach(x => ls[x]++);
-                for (int i = 0; i < rs.Count; i++)
-                {
-                    rs[i]++;
-                }
+                    for (int i = 0; i < ls.Count; i++)
+                    {
+                        ls[i]--;
+                    }
 
-                //Find dupes
-                List<int> dupes = ls.Where(x => rs.Contains(x)).ToList();
+                    //Increment R's
+                    //rs.ForEach(x => ls[x]++);
+                    for (int i = 0; i < rs.Count; i++)
+                    {
+                        rs[i]++;
+                    }
 
-                //Remove dupes
-                ls.RemoveAll(x => dupes.Contains(x));
-                rs.RemoveAll(x => dupes.Contains(x));
+                    //Find dupes
+                    List<int> dupes = ls.Where(x => rs.Contains(x)).ToList();
 
-                //Remove out of range indeces.
-                ls.RemoveAll(x => x < 0);
-                rs.RemoveAll(x => x >= dominoList.Count);
+                    //Remove dupes
+                    ls.RemoveAll(x => dupes.Contains(x));
+                    rs.RemoveAll(x => dupes.Contains(x));
 
-                //Remove fallen dominoes.
-                ls.RemoveAll(x => dominoList[x].Direction != '.');
-                rs.RemoveAll(x => dominoList[x].Direction != '.');
+                    //Remove out of range indeces.
+                    ls.RemoveAll(x => x < 0);
+                    rs.RemoveAll(x => x >= dominoList.Count);
 
-                //replace
-                ls.ForEach(x => { if (dominoList[x].Direction == '.') dominoList[x].Direction = 'L'; });
-                rs.ForEach(x => { if (dominoList[x].Direction == '.') dominoList[x].Direction = 'R'; });
-            } while (ls.Count != 0 || rs.Count != 0);
+                    //Remove fallen dominoes.
+                    ls.RemoveAll(x => dominoList[x].Direction != '.');
+                    rs.RemoveAll(x => dominoList[x].Direction != '.');
 
-
-            Console.WriteLine();
-            Console.Write("After: ");
-            dominoList.ForEach(x => Console.Write(x.Direction));
-
-
-            Console.ReadKey();
+                    //replace
+                    ls.ForEach(x => { if (dominoList[x].Direction == '.') dominoList[x].Direction = 'L'; });
+                    rs.ForEach(x => { if (dominoList[x].Direction == '.') dominoList[x].Direction = 'R'; });
+                } while (ls.Count != 0 || rs.Count != 0);
 
 
+                Console.WriteLine();
+                Console.Write("After: ");
+                dominoList.ForEach(x => Console.Write(x.Direction));
 
 
-            /*
-            Track current index
-            three possible cases
-            '.' is the current position.
-            
-            1. L . . or . . R or R . L => Unchanged (does not matter if the dominoes are always falling away <- . ->)
-            2. R . => . -> R
-            3. . L => . -> L 
-
-            Example:
-            .L.R....L
-            LL.RR..LL
-            LL.RRRLLL
-
-            Find "L{...}R" and split on that. 
-
-
-            How do I loop each step?
-            How do I know when I'm done?
-*/
-            
-
+                Console.ReadKey();
+            }
         }
     }
 }
